637223d9452147a15a3b1c2c8724b53c5ce69530
wesleybits <wesley.bitomski@incrowdnow.com>
2018-01-04 18:20:39 -0500

    quoted deps list

--- xfunctions-doc/info.rkt
+++ xfunctions-doc/info.rkt
@@ -3,7 +3,7 @@
 (define name "xfunctions-doc")
 (define version "0.1")
 (define deps
-  (("sandbox-lib")
-   ("scribble-lib")
-   ("racket-doc")
-   ("xfunctions-lib")))
+  '(("sandbox-lib")
+    ("scribble-lib")
+    ("racket-doc")
+    ("xfunctions-lib")))
